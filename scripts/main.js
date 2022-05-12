63775e62ae13a73be4f477dfbda13caf3414bb65
Levi Shevick <levidshevick@gmail.com>
2022-05-12 13:04:04 -0400

    adds css to button and adds timeout functions to alert status of war

--- scripts/main.js
+++ scripts/main.js
@@ -11,10 +11,11 @@
     const $refreshMessage = document.querySelector('.refresh');
     const $playerAmount = document.querySelector('.player-card-amount');
     const $robotAmount = document.querySelector('.robot-card-amount');
+    const $arena = document.querySelector('#contest');
 
     const cardNames = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
     const cardSuits = ['Hearts', 'Diamonds', 'Spades', 'Clubs'];
-    const constestArr = [];
+    const contestArr = [];
 
 
     // this is the card constructor
@@ -38,17 +39,13 @@
 
     }
     // this is the player constructor
-    function Player({ name, hand = [], score = 0 }) {
+    function Player(name) {
         this.name = name;
-        this.hand = hand;
-        this.score = score;
-        // players need to have a name to identify them, and a hand to hold the cards they are dealt
+        this.hand = [];
     }
     // this is the game constructor
-    function Game({ players = [], play = [] } = {}) {
-        this.players = players;
-        this.play = play;
-        // game constructor needs to have the players playing the game, and a shuffle function to randomize cards
+    function Game() {
+        this.players = [];
     }
     //--------------------------------------------------------------------------------------------------------------------//
 
@@ -58,14 +55,9 @@
     Game.prototype.start = function () {
         this.players.push(new Player({ name: 'you' }));
         this.players.push(new Player({ name: 'BeepBoop' }));
-        // console.log(this.players);
+
         const deck = new Deck({ names: cardNames, suits: cardSuits });
         this.shuffle(deck);
-        // console.log(deck);
-        // this.players[0].hand = deck.slice(0, 26);
-        // this.players[1].hand = deck.slice(26, 52);
-        // console.log(this.players[0].hand);
-        // console.log(this.players[1].hand);
         this.deal(deck);
     };
     Game.prototype.shuffle = function (deck) {
@@ -76,8 +68,8 @@
             i = Math.floor(Math.random() * m--);
             [deck[m], deck[i]] = [deck[i], deck[m]];
         }
-        }
-    
+    }
+
 
     Game.prototype.deal = function (deck) {
         for (let i = (deck.length / 2); i--;) {
@@ -93,7 +85,6 @@
     newGame.start();
     const playerOne = newGame.players[0];
     const robot = newGame.players[1];
-    console.log(playerOne, robot);
     // now what to do with these shuffled cards?? we need to play the game.... 
     // we need to compare the two values of cards on the top of the decks
     // whichever card is higher, that player takes both cards
@@ -116,6 +107,8 @@
             robot.hand.shift();
             playerOne.hand.shift();
         } else {
+            $playButton.disabled = true;
+            $playButton.textContent = 'WAR!!'
             war();
         }
     }
@@ -137,13 +130,15 @@
             length = 3;
         }
         for (let i = 0; i < length; i++) {
-            constestArr.push(playerOne.hand[0]);
+            contestArr.push(playerOne.hand[0]);
             playerOne.hand.shift();
-            constestArr.push(robot.hand[0]);
+            contestArr.push(robot.hand[0]);
             robot.hand.shift();
-
         }
-        compareWar(playerOne.hand[0], robot.hand[0]);
+        updateWarStatus();
+        setTimeout(() => {
+            compareWar(playerOne.hand[0], robot.hand[0]);
+        }, 3000);
     }
 
     function compareWar(player, computer) {
@@ -152,50 +147,70 @@
         let rValue = computer.value
         // when war is called check values of top cards to determine winner
         if (pValue > rValue) {
-            playerOne.hand.push.apply(playerOne.hand, constestArr);
+            playerOne.hand.push.apply(playerOne.hand, contestArr);
             playerOne.hand.push(robot.hand[0]);
             playerOne.hand.push(playerOne.hand[0]);
             playerOne.hand.shift();
             robot.hand.shift();
+            $arena.textContent = `You took all the cards!!! 🤩`;
         } else if (pValue < rValue) {
-            robot.hand.push.apply(robot.hand, constestArr);
+            robot.hand.push.apply(robot.hand, contestArr);
             robot.hand.push(robot.hand[0]);
             robot.hand.push(playerOne.hand[0]);
             playerOne.hand.shift();
             robot.hand.shift();
+            $arena.textContent = '🤖 took your cards!!! 😵‍💫';
         } else {
             war();
         }
-        constestArr.length = 0;
+        contestArr.length = 0;
+        setTimeout(() => {
+            $playButton.disabled = false;
+            $playButton.textContent = 'Play!!!'
+            revertWarStatus();
+        }, 2000);
     }
 
-    
+
     function checkIfOut() {
         //checks if either player is out of cards
         if (playerOne.hand.length === 0) {
-        $playerAmount.innerHTML = '😢';
+            $playerAmount.innerHTML = '😢';
+            $playerCard.value = '';
             $robotCard.value = 'I WIN HAHA';
             $playButton.classList.add('hidden');
             $refreshMessage.classList.remove('hidden');
-            return true;
+            return;
         } else if (robot.hand.length === 0) {
-        $robotAmount.innerHTML = '🤖: sad beep boop';
+            $robotAmount.innerHTML = '🤖: sad beep boop';
+            $robotCard.value = '';
             $playerCard.value = 'You Win!'
             $playButton.classList.add('hidden');
             $refreshMessage.classList.remove('hidden');
-            return true;
+            return;
         } else {
-            return false;
+            return;
         }
     }
-    
+
     $playButton.onclick = function () {
         compare(playerOne.hand[0], robot.hand[0]);
         $playerCard.value = `${playerOne.hand[0].name} of ${playerOne.hand[0].suit}`;
         $robotCard.value = `${robot.hand[0].name} of ${robot.hand[0].suit}`;
         $playerAmount.innerHTML = `${playerOne.hand.length}`;
         $robotAmount.innerHTML = `${robot.hand.length}`;
-        console.log(playerOne.hand.length);
-        console.log(robot.hand.length);
     }
+
+    // ======================== CALLBACK FUNCTIONS ===================================//
+
+    function updateWarStatus() {
+        $arena.classList.remove('hidden');
+        $arena.textContent = 'Cards have been added to the WAR arena!';
+    }
+
+    function revertWarStatus() {
+        $arena.classList.add('hidden');
+        $arena.textContent = '';
+    }
+
 })(); 
